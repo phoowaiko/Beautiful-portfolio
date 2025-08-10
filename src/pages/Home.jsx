6862aa8d4be863f1cbad18618fa2e9a00c17b266
Phoo Wai Ko <phoowaiko0@gmail.com>
2025-08-10 20:45:41 +0630

    addAbout

---
+++ src/pages/Home.jsx
@@ -0,0 +1,29 @@
+import StarBackground from "@/components/StarBackground";
+import ThemeToggle from "../components/ThemeToggle";
+import Navbar from "../components/Navbar";
+import HeroSection from "../components/HeroSection";
+import AboutSection from "../components/AboutSection";
+
+const Home = () => {
+  return (
+    <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
+      {/* Theme Toggle */}
+      <ThemeToggle />
+
+      {/* Background Effects */}
+      <StarBackground />
+
+      {/* Navbar */}
+      <Navbar />
+
+      {/* Main Content */}
+      <main>
+        <HeroSection />
+        <AboutSection />
+      </main>
+      {/* footer */}
+    </div>
+  );
+};
+
+export default Home;
