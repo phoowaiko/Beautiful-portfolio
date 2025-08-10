a2e0eb516617d04a4bea8d669697299771427056
Phoo Wai Ko <phoowaiko0@gmail.com>
2025-08-11 00:22:04 +0630

    addskillssection

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,7 @@ import ThemeToggle from "../components/ThemeToggle";
 import Navbar from "../components/Navbar";
 import HeroSection from "../components/HeroSection";
 import AboutSection from "../components/AboutSection";
+import SkillsSection from "../components/SkillsSection";
 
 const Home = () => {
   return (
@@ -20,6 +21,7 @@ const Home = () => {
       <main>
         <HeroSection />
         <AboutSection />
+        <SkillsSection />
       </main>
       {/* footer */}
     </div>
